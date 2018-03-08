50c9f097407059b829a5223e3867dfcad15c3cdc
Thomas Castiglione <castiglione@gmail.com>
2018-03-08 18:55:55 +0800

    call google translate

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Google.Cloud.Translation.V2;
 
 namespace i5n
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var client = TranslationClient.Create();
+            var result = await client.TranslateTextAsync(args[0], LanguageCodes.Italian);
+            Console.WriteLine(result.TranslatedText);
         }
     }
 }
